c2d87ab67a7eff14e80ae6c59ac4b8b7d4855913
AnayaJohn <64412959+AnayaJohn@users.noreply.github.com>
2021-12-29 17:15:33 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBBwmPFkFAsqhL6D3LaQv5odawJtpk-Yng",
+    authDomain: "project-83-40b95.firebaseapp.com",
+    projectId: "project-83-40b95",
+    storageBucket: "project-83-40b95.appspot.com",
+    messagingSenderId: "630349309694",
+    appId: "1:630349309694:web:34f3530c814acc340427af"
+  };
